99efa3dd06230ff8a8836a52eecbfeca3c7fd796
Andrei Harja <harjaandrei@yahoo.com>
2026-04-19 19:34:02 +0300

    Added back button to Settings Page and changed colors for Planner Page (Untested)

--- Views/SettingsPage.xaml.cs
+++ Views/SettingsPage.xaml.cs
@@ -9,4 +9,22 @@ public partial class SettingsPage : ContentPage
         InitializeComponent();
         BindingContext = viewModel;
     }
+
+    private async void OnBackButtonTapped(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync("..");
+    }
+
+    private async void OnTermsTapped(object sender, EventArgs e)
+    {
+        try
+        {
+            Uri uri = new Uri("https://github.com/aandreis20/project_pdm");
+            await Browser.Default.OpenAsync(uri, BrowserLaunchMode.SystemPreferred);
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Error", "Could not open the link.", "OK");
+        }
+    }
 }
